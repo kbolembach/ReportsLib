742c41592a3ac36ae25ce885028e055b8331757d
kbolembach <konrad.bolembach@gmail.com>
2022-12-07 13:57:34 +0100

    added style specification for plotting error bars

--- lib/PlotErrorBars.m
+++ lib/PlotErrorBars.m
@@ -1,10 +1,17 @@
-function PlotErrorBars(qx, qy)
+function PlotErrorBars(qx, qy, nvargs)
 %PLOTERRORBARS Funkcja pomocnicza do rysowania errorbarów na wykresie
 %wielkości qx, qy. Errorbary reprezentują niepewności tych wartości.
+
+arguments
+   qx
+   qy
+   nvargs.style = 'k.'
+end
+
 x = GetValue(qx, 1);
 y = GetValue(qy, 1);
 xerr = GetUncertainty(qx, 1);
 yerr = GetUncertainty(qy, 1);
-errorbar(x, y, yerr, yerr, xerr, xerr, 'k.');
+errorbar(x, y, yerr, yerr, xerr, xerr, nvargs.style);
 end
 
